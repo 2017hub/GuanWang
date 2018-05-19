13e453d5819439f4f12cf28f13b9ed1217d9f404
longyaoyuan <longyaoyuan1987@163.com>
2018-05-19 17:30:58 +0800

    增加request请求参数的获取方式

--- src/main/java/com/guanwang/controller/HelloController.java
+++ src/main/java/com/guanwang/controller/HelloController.java
@@ -5,14 +5,18 @@ import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
-    @RequestMapping("/hello")
-    public String printHello(Model model) {
+   @RequestMapping(value = "/mvc",method =RequestMethod.GET)
+    public String printHello(@RequestParam("userId") Integer userId, Model model) {
+       System.out.println("输出---ID--"+userId);
         System.out.println("--------------hello--------------");
        // model.addAttribute("message", "Hello Spring MVC Framework!");
         model.addAttribute("message","hello------------------");
