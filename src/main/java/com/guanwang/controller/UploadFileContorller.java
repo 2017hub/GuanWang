14c5e30ef38fccec851076770f10af59fb146d4c
longyaoyuan <longyaoyuan1987@163.com>
2018-05-26 17:50:01 +0800

    添加单个文件上传时候动态获取上传的路径upload,IDEA不加载空文件夹

--- src/main/java/com/guanwang/controller/UploadFileContorller.java
+++ src/main/java/com/guanwang/controller/UploadFileContorller.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.io.IOException;
+
 
 
 /**
@@ -17,17 +18,22 @@ import java.io.IOException;
 @Controller
 public class UploadFileContorller {
     @RequestMapping(value = "/sigonFile",method =RequestMethod.POST)
-    public String uploadFile(@RequestParam("bigHeadImage")MultipartFile file) throws IOException {
+    public String uploadFile(@RequestParam("bigHeadImage")MultipartFile file, HttpServletRequest request) throws Exception {
             //getOriginalFilename()方法是获取上传文件的文件名
        String originalFilename= file.getOriginalFilename();
         System.out.println("------>"+originalFilename);
         //getName()方法是获取上传文件jsp<input>file控件的name值
         System.out.println("------>"+file.getName());
+        //动态获取上传路径
+       String path= request.getServletContext().getRealPath("/upload");
+        System.out.println("----->"+path);
+      // File  uploadPath=new File("D:\\upload\\"+originalFilename);
+        File  uploadPath=new File(path+"\\"+originalFilename);
+      //  File  uploadPath=new File(" D:\\develop\\GuanWang\\lake\\src\\main\\webapp\\upload\\"+originalFilename);
 
-        File  filePath=new File("D:\\upload\\"+originalFilename);
-
+        System.out.println("------>"+uploadPath);
         //将文件上传到指定的路径中
-        file.transferTo(filePath);
+        file.transferTo(uploadPath);
         return "success";
     }
 }
